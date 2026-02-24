7b702bfc8b85ced4ad8a8754a6c49598cb08b97b
apolo-matos <apollomatos22@gmail.com>
2026-02-24 16:58:06 -0300

    ajustes no leyout de curso

--- site-faama/src/components/Curso.js
+++ site-faama/src/components/Curso.js
@@ -2,36 +2,18 @@ import React from 'react';
 import '../styles/Curso.css';
 import educacaoImagem from '../Assets/Group 62.png';
 import fotoAluna from '../Assets/Ellipse 1.png';
-import img1 from '../Assets/Group 53.png';
 import seta from '../Assets/weui_arrow-filled.png';
-import img2 from '../Assets/Group 56.png';
-import img3 from '../Assets/Group 55.png';
-import img4 from '../Assets/Group 57.png';
-import img5 from '../Assets/Group 54.png';
 
 const Curso = () => {
-  const listaCursos = [
-    { id: 1, nome: "DIREI TO", img: img1, pos: "baixo" },
-    { id: 2, nome: "PEDA GOGIA", img: img2, pos: "cima" },
-    { id: 3, nome: "A.D.S.", img: img3, pos: "baixo" },
-    { id: 4, nome: "PSICO LOGIA", img: img4, pos: "cima" },
-    { id: 5, nome: "TEO LOGIA", img: img5, pos: "baixo"}
-  ];
-
   return (
-    <section className="container-cursos">
-      <h1 className="curso-secao">Cursos</h1>
-      <div className="grupo-cards">
-      {listaCursos.map((curso) => (
-        <div 
-        key={curso.id} 
-        className={`card-curso ${curso.pos}`}
-        style={{ backgroundImage: `url(${curso.img})` }}
-        >
-          <strong>{curso.nome}</strong>
-          </div>
-      ))}
-      </div>
+    <section className="container">
+      <div className="container-cursos">
+        <div className="card cima card-direito"></div>
+        <div className="card baixo card-teologia"></div>
+        <div className="card cima card-ads"></div>
+        <div className="card baixo card-pedagogia"></div>
+        <div className="card cima card-psicologia"></div>
+        </div>
          <div className="educacao-container">
            <img src={educacaoImagem} alt="Destaque" className="imagem-educacao"/>
           <button className="botao-saiba-mais">saiba mais</button>
@@ -49,7 +31,6 @@ const Curso = () => {
             <img src={seta} alt="proxima página"/>
           </button>
           </div>
-          
     </section>
   );
 };
