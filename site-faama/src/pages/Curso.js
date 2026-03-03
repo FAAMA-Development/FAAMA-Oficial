9a6525cd0b3084b6a5f73456b71af40ad0d4c9e2
adriellrochaFaama <adriellrocha@hotmail.com>
2026-03-03 13:28:27 -0300

    Format Curso.js and remove unused React import
    Remove the unused `React` import (using the modern JSX runtime), add a semicolon to the CSS import, and tidy JSX indentation/nesting in Curso.js for clearer structure. No functional changes.

--- site-faama/src/pages/Curso.js
+++ site-faama/src/pages/Curso.js
@@ -1,24 +1,23 @@
-import React from 'react';
 import Navbar from '../components/Navbar';
-import "../styles/Curso.css"
+import "../styles/Curso.css";
 
 function Curso() {
-    return(
+    return (
         <>
-        <Navbar />
-        <section className='container'>
-            <div>
-                <h1>CURSOS</h1>
-                <p>Cursos ofertados na Faculdade Adventista da Amazônia</p>
-                <div className="container-cursos">
-                <div className="card card-direito"></div>
-                <div className="card card-teologia"></div>
-                <div className="card card-ads"></div>
-                <div className="card card-pedagogia"></div>
-                <div className="card card-psicologia"></div>
+            <Navbar />
+            <section className='container'>
+                <div>
+                    <h1>CURSOS</h1>
+                    <p>Cursos ofertados na Faculdade Adventista da Amazônia</p>
+                    <div className="container-cursos">
+                        <div className="card card-direito"></div>
+                        <div className="card card-teologia"></div>
+                        <div className="card card-ads"></div>
+                        <div className="card card-pedagogia"></div>
+                        <div className="card card-psicologia"></div>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
         </>
     )
 };
